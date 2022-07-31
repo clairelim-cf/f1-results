de308a42635dcebd5405bf74133803626cb63225
Claire Lim <clairelim@cloudflare.com>
2022-08-01 00:12:59 +0800

    first commit

---
+++ scripts/f1standingData.js
@@ -0,0 +1,189 @@
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Max Verstappen VER</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>258</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Charles Leclerc LEC</td>
+      <td>Ferrari</td>
+      <td>178</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Sergio Perez PER</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>173</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>George Russell RUS</td>
+      <td>Mercedes</td>
+      <td>158</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Carlos Sainz SAI</td>
+      <td>Ferrari</td>
+      <td>156</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Lewis Hamilton HAM</td>
+      <td>Mercedes</td>
+      <td>146</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Lando Norris NOR</td>
+      <td>McLaren Mercedes</td>
+      <td>76</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Esteban Ocon OCO</td>
+      <td>Alpine Renault</td>
+      <td>58</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Valtteri Bottas BOT</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>46</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Fernando Alonso ALO</td>
+      <td>Alpine Renault</td>
+      <td>41</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Kevin Magnussen MAG</td>
+      <td>Haas Ferrari</td>
+      <td>22</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>Daniel Ricciardo RIC</td>
+      <td>McLaren Mercedes</td>
+      <td>19</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>Pierre Gasly GAS</td>
+      <td>AlphaTauri RBPT</td>
+      <td>16</td>
+    </tr>
+    <tr>
+      <td>14</td>
+      <td>Sebastian Vettel VET</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>16</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>Mick Schumacher MSC</td>
+      <td>Haas Ferrari</td>
+      <td>12</td>
+    </tr>
+    <tr>
+      <td>16</td>
+      <td>Yuki Tsunoda TSU</td>
+      <td>AlphaTauri RBPT</td>
+      <td>11</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>Zhou Guanyu ZHO</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td>18</td>
+      <td>Lance Stroll STR</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>Alexander Albon ALB</td>
+      <td>Williams Mercedes</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>20</td>
+      <td>Nicholas Latifi LAT</td>
+      <td>Williams Mercedes</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>21</td>
+      <td>Nico Hulkenberg HUL</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
+`;
+document.getElementById('f1-drivers').innerHTML+=tabledata;
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Red Bull Racing RBPT</td>
+      <td>431</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Ferrari</td>
+      <td>334</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Mercedes</td>
+      <td>304</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Alpine Renault</td>
+      <td>99</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>McLaren Mercedes</td>
+      <td>95</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Alfa Romeo Ferrari</td>
+      <td>51</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Haas Ferrari</td>
+      <td>34</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>AlphaTauri RBPT</td>
+      <td>27</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Aston Martin Aramco Mercedes</td>
+      <td>20</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Williams Mercedes</td>
+      <td>3</td>
+    </tr>
+  </tbody>
+</table>
+`;
+document.getElementById('f1-constructors').innerHTML+=tabledata;
+document.getElementById('lastup').innerHTML='31/07/2022 16:04:04 UTC';
---
+++ scripts/f2standingData.js
@@ -0,0 +1,199 @@
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>F. Drugovich</td>
+      <td>MP Motorsport</td>
+      <td>180</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>T. Pourchaire</td>
+      <td>ART Grand Prix</td>
+      <td>159</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>L. Sargeant</td>
+      <td>Carlin</td>
+      <td>119</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>E. Fittipaldi</td>
+      <td>Charouz Racing System</td>
+      <td>100</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>J. Daruvala</td>
+      <td>PREMA Racing</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>F. Vesti</td>
+      <td>ART Grand Prix</td>
+      <td>91</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>A. Iwasa</td>
+      <td>DAMS</td>
+      <td>90</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>L. Lawson</td>
+      <td>Carlin</td>
+      <td>88</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>J. Doohan</td>
+      <td>Virtuosi Racing</td>
+      <td>88</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>J. Vips</td>
+      <td>Hitech Grand Prix</td>
+      <td>85</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>M. Armstrong</td>
+      <td>Hitech Grand Prix</td>
+      <td>79</td>
+    </tr>
+    <tr>
+      <td>12</td>
+      <td>D. Hauger</td>
+      <td>PREMA Racing</td>
+      <td>67</td>
+    </tr>
+    <tr>
+      <td>13</td>
+      <td>R. Verschoor</td>
+      <td>Trident</td>
+      <td>49</td>
+    </tr>
+    <tr>
+      <td>15</td>
+      <td>C. Novalak</td>
+      <td>MP Motorsport</td>
+      <td>26</td>
+    </tr>
+    <tr>
+      <td>17</td>
+      <td>R. Nissany</td>
+      <td>DAMS</td>
+      <td>18</td>
+    </tr>
+    <tr>
+      <td>18</td>
+      <td>R. Merhi</td>
+      <td>Campos Racing</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>19</td>
+      <td>O. Caldwell</td>
+      <td>Campos Racing</td>
+      <td>8</td>
+    </tr>
+    <tr>
+      <td>20</td>
+      <td>M. Sato</td>
+      <td>Virtuosi Racing</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>21</td>
+      <td>C. Williams</td>
+      <td>Trident</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td>22</td>
+      <td>D. Beckmann</td>
+      <td>Van Amersfoort Racing</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>23</td>
+      <td>C. Bölükbasi</td>
+      <td>Charouz Racing System</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>24</td>
+      <td>A. Cordeel</td>
+      <td>Van Amersfoort Racing</td>
+      <td>0</td>
+    </tr>
+  </tbody>
+</table>
+`;
+document.getElementById('f2-drivers').innerHTML+=tabledata;
+var tabledata=`<table border="1" class="dataframe">
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>ART Grand Prix</td>
+      <td>250</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Carlin</td>
+      <td>207</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>MP Motorsport</td>
+      <td>206</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Hitech Grand Prix</td>
+      <td>164</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>PREMA Racing</td>
+      <td>161</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>DAMS</td>
+      <td>108</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Charouz Racing System</td>
+      <td>104</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Virtuosi Racing</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td>Campos Racing</td>
+      <td>57</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td>Trident</td>
+      <td>54</td>
+    </tr>
+    <tr>
+      <td>11</td>
+      <td>Van Amersfoort Racing</td>
+      <td>26</td>
+    </tr>
+  </tbody>
+</table>
+`;
+document.getElementById('f2-constructors').innerHTML+=tabledata;
